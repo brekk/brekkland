2bf292e296cefa4b32cc818d309a6fdd7dfe57c1
brekk <brekk@brekkbockrath.com>
2026-05-07 11:11:58 -0700

    chore: some testing

---
+++ stylelint.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: ['stylelint-config-standard-scss'],
+  rules: {
+    'scss/percent-placeholder-pattern': null,
+    'scss/dollar-variable-pattern': null,
+    'scss/at-mixin-pattern': null,
+    'keyframes-name-pattern': null,
+    'selector-class-pattern': null,
+  },
+}
