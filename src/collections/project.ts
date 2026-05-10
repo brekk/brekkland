faee5280b9735cc6ea946c8051fdf49d08948e9f
brekk <brekk@brekkbockrath.com>
2026-05-09 17:08:39 -0700

    chore(use bun): remove yarn and try bun

---
+++ src/collections/project.ts
@@ -0,0 +1,32 @@
+import { defineCollection } from "astro:content"
+
+import { glob } from "astro/loaders"
+import { z } from "astro/zod"
+
+export default defineCollection({
+  // Load Markdown and MDX files in the `src/content/projects/` directory.
+  loader: glob({
+    base: "./src/content/projects",
+    pattern: "**/*.{md,mdx} ",
+  }),
+  schema: ({ image }) =>
+    z.object({
+      repo: z.string(),
+      title: z.string(),
+      description: z.string(),
+      version: z.string(),
+      isPublished: z.boolean(),
+      hasBinary: z.boolean(),
+      meta: z.object({
+        date: z.object({
+          // Transform string to Date object
+          published: z.coerce.date(),
+          updated: z.coerce.date().optional(),
+        }),
+      }),
+      image: z.optional(image()),
+      dependencies: z.optional(z.array(z.string())),
+      related: z.optional(z.array(z.string())),
+      tags: z.optional(z.array(z.string())),
+    }),
+})
