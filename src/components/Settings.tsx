fb36e2ff63eded97d3627046b4e2e4dc06a360ea
brekk <brekk@brekkbockrath.com>
2026-06-05 16:04:38 -0700

    chore(Settings): slice apart Settings to see if this addresses the mobile issues we're seeing

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,35 +1,74 @@
 import "@/components/Settings.scss"
 import { SlidersVertical } from "lucide-react"
-import { ColorBoxFg, ColorBoxBg, ColorBoxAccent } from "@/components/ColorBox.tsx"
+import {
+  ColorBoxFg,
+  ColorBoxBg,
+  ColorBoxAccent,
+} from "@/components/ColorBox.tsx"
 import blem from "#/utilities/blem.ts"
 import { useState } from "react"
+import { $settingsVisible } from "@/stores/settings"
+import { useStore } from "@nanostores/react"
 
 const bem = blem("Settings")
-export const Settings = () => {
-  const [$visible, $setVisible] = useState(false)
-  const activeClass = [$visible ? "active" : "inactive"]
-  const toggle = (e) => {
 
+interface SubProps {
+  activeClass: string[]
+  toggle: (e: React.MouseEvent<unknown>) => void
+}
+
+const useSettingsStore = () => {
+  const isVisible = useStore($settingsVisible)
+
+  const activeClass = [isVisible ? "active" : "inactive"]
+  const toggle = (e: React.MouseEvent<unknown>) => {
     e.preventDefault()
-    $setVisible(!$visible)
+    $settingsVisible.set(!isVisible)
   }
+  return { isVisible, toggle, activeClass }
+}
+
+export const DumbSettingsPane = ({ activeClass, toggle }: SubProps) => (
+  <div className={bem("pane", activeClass)}>
+    <SlidersVertical className={bem("context-indicator")} onClick={toggle} />
+    <div className={bem("wrapper", activeClass)}>
+      <ColorBoxFg />
+      <ColorBoxBg />
+      <ColorBoxAccent />
+    </div>
+  </div>
+)
+
+const DumbSettingsToggle = ({ toggle, activeClass }: SubProps) => (
+  <button
+    className={bem("button", [...activeClass, "settings"])}
+    onClick={toggle}
+  >
+    <SlidersVertical />
+  </button>
+)
+
+export const SettingsToggle = () => {
+  const $settings = useSettingsStore()
+  return (
+    <DumbSettingsToggle {...$settings} />
+  )
+}
+
+export const SettingsPane = () => {
+  const $settings = useSettingsStore()
+  return (
+    <DumbSettingsPane {...$settings} />
+  )
+}
+
+export const Settings = () => {
+  const $settings = useSettingsStore()
   return (
     <div className={bem("")}>
-      <button
-        className={bem("button", [...activeClass, "settings"])}
-        onClick={toggle}
-      >
-        <SlidersVertical />
-      </button>
-      <div className={bem("pane", activeClass)} onClick={() => /*$setVisible(false)}>*/ { }}>
-        <SlidersVertical className={bem("context-indicator")} onClick={toggle} />
-        <div className={bem("wrapper", activeClass)}>
-          <ColorBoxFg />
-          <ColorBoxBg />
-          <ColorBoxAccent />
-        </div>
-      </div>
-    </div >
+      <DumbSettingsToggle {...$settings} />
+      <DumbSettingsPane {...$settings} />
+    </div>
   )
 }
 
