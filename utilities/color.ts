e584d5ef18e66d7622a7e15817c3c46ab1626619
brekk <brekk@brekkbockrath.com>
2026-06-05 23:15:12 -0700

    chore: some swup tests here

--- utilities/color.ts
+++ utilities/color.ts
@@ -2,11 +2,11 @@ import { propOr, __ as $, pipe } from "ramda"
 
 type ColorTrio = [string, string, string]
 
-export const DEFAULT_PALETTE = "armyGavy"
+export const DEFAULT_PALETTE = "neonDevil"
 export const PALETTE: Record<string, ColorTrio> = {
   cypherpunk: ["#000", "#ecf832", "#fff"],
   neonDevil: ["#ffc300", "#cf0015", "#2e2e35"],
-  armyGavy: ["#cdb855", "#431e84", "#638d6f"],
+  armyGavy: ["#d5db98", "#240260", "#5b7b64"],
 }
 
 export const paletteToObject = ([fg, accent, bg]: ColorTrio) => ({
