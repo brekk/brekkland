3be2b193754846e631d0987cfb7f988a7df52bbc
brekk <brekk@brekkbockrath.com>
2026-06-07 17:55:07 -0700

    chore: wire colorable and icons

--- src/components/ColorBox.tsx
+++ src/components/ColorBox.tsx
@@ -1,20 +1,39 @@
 import "@/components/ColorBox.scss"
-import type { PreinitializedWritableAtom } from "nanostores"
+import type { PreinitializedWritableAtom, Batched } from "nanostores"
 import React, { useCallback, useRef, useState } from "react"
 import { HexColorPicker } from "react-colorful"
+import type { Accessibility } from "#/colorable.d.ts"
 
 import { useClickAway } from "@uidotdev/usehooks"
 import type { ChangeEvent } from "react"
+import { Medal, Trophy } from "lucide-react"
 import blem from "#/utilities/blem.ts"
 
-import { $bg, $fg, $accent, $palette } from "@/stores/colors"
+import {
+  $bg,
+  $fg,
+  $accent,
+  $palette,
+  $contrastBgAccent,
+  $contrastFgAccent,
+  $contrastFgBg,
+} from "@/stores/colors"
 import { useStore } from "@nanostores/react"
 const bem = blem("ColorBox")
 
+type Comp = Batched<Accessibility, string>
+
 export const makePicker =
-  (short: string, store: PreinitializedWritableAtom<string>, label: string) =>
+  (
+    short: string,
+    store: PreinitializedWritableAtom<string>,
+    label: string,
+    [comp1, comp2]: [Comp, Comp],
+  ) =>
     () => {
       const $color = useStore(store)
+      const $comp1 = useStore(comp1)
+      const $comp2 = useStore(comp2)
       const [$isOpen, $setIsOpen] = useState(false)
 
       const ref = useClickAway(() => {
@@ -27,14 +46,31 @@ export const makePicker =
         hatemail?.style.setProperty(varName, newColor)
         document.getElementById("body")?.style.setProperty(varName, newColor)
       }
+      const mods = [
+        short,
+        $comp1.aa && $comp2.aa ? "aa" : "no-aa",
+        $comp1.aaLarge && $comp2.aaLarge ? "aa-large" : "no-aa-large",
+        $comp1.aaa && $comp2.aaa ? "aaa" : "no-aaa",
+        $comp1.aaaLarge && $comp2.aaaLarge ? "aaa-large" : "no-aaa-large",
+      ]
 
       return (
-        <div className={bem("picker", [short])} id={`color-${short}`}>
+        <div className={bem("picker", mods)} id={`color-${short}`}>
           <div
             className={bem("swatch", [short])}
             style={{ backgroundColor: $color }}
             onClick={() => $setIsOpen(true)}
-          />
+          >
+            {$comp2.aaa || $comp1.aaa || $comp2.aaaLarge || $comp1.aaaLarge ? (
+              <Trophy
+                className={bem("aaa", [$comp1.aaaLarge ? "large" : "regular"])}
+              />
+            ) : $comp2.aa || $comp1.aa || $comp2.aaLarge || $comp1.aaLarge ? (
+              <Medal
+                className={bem("aa", [$comp1.aaLarge ? "large" : "regular"])}
+              />
+            ) : null}
+          </div>
 
           {$isOpen && (
             <div className={bem("popover")} ref={ref as any}>
@@ -47,16 +83,25 @@ export const makePicker =
     }
 
 export const ColorBoxBg = () => {
-  const C = makePicker("bg", $bg, "Background")
+  const C = makePicker("bg", $bg, "Background", [
+    $contrastFgBg,
+    $contrastBgAccent,
+  ])
   return <C />
 }
 
 export const ColorBoxFg = () => {
-  const C = makePicker("fg", $fg, "Foreground")
+  const C = makePicker("fg", $fg, "Foreground", [
+    $contrastFgBg,
+    $contrastFgAccent,
+  ])
   return <C />
 }
 
 export const ColorBoxAccent = () => {
-  const C = makePicker("accent", $accent, "Accent")
+  const C = makePicker("accent", $accent, "Accent", [
+    $contrastFgAccent,
+    $contrastBgAccent,
+  ])
   return <C />
 }
--- src/stores/colors.ts
+++ src/stores/colors.ts
@@ -1,5 +1,12 @@
-import { atom } from "nanostores"
+import { atom, batched } from "nanostores"
 import { DEFAULT_PALETTE_RECORD, DEFAULT_PALETTE } from "#/utilities/color"
+import colorable from "colorable"
+import { pipe, nth, prop } from "ramda"
+import type {
+  HexContrast,
+  CompactComparison,
+  Accessibility,
+} from "#/colorable.d.ts"
 
 export const colorFg = atom<string>(DEFAULT_PALETTE_RECORD.fg)
 export const $fg = colorFg
@@ -7,6 +14,15 @@ export const colorAccent = atom<string>(DEFAULT_PALETTE_RECORD.accent)
 export const $accent = colorAccent
 export const colorBg = atom<string>(DEFAULT_PALETTE_RECORD.bg)
 export const $bg = colorBg
+
+const compareColors = (a: string, b: string): Accessibility => {
+  const [c] = colorable([a, b], { compact: true, threshold: 0 })
+  return c.combinations[0].accessibility
+}
+
+export const $contrastFgBg = batched([$fg, $bg], compareColors)
+export const $contrastFgAccent = batched([$fg, $accent], compareColors)
+export const $contrastBgAccent = batched([$bg, $accent], compareColors)
 
 export const colorPalette = atom<string>(DEFAULT_PALETTE)
 export const $palette = colorPalette
